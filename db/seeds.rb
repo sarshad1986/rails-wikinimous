f4f41613b547b48539669185b42a8f1570898f5c
Sarah Arshad <sarshad1@yahoo.co.uk>
2022-05-25 10:51:56 +0100

    wiki challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  Article.create(title: Faker::Fantasy::Tolkien.character, content: Faker::Movies::Hobbit.location)
+end
